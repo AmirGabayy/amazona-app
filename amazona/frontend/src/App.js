318dbb93a9bd6b360bf29a436a7b3161427ab0b5
Amir Gabay <amir2347@gmail.com>
2022-07-20 19:32:01 -0400

    cart handling

--- amazona/frontend/src/App.js
+++ amazona/frontend/src/App.js
@@ -1,29 +1,43 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import CartScreen from './screens/CartScreen';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
+import React from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import CartScreen from "./screens/CartScreen";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a href="/" className="brand">amazona</a>
+            <Link to="/" className="brand">
+              amazona
+            </Link>
           </div>
           <div>
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign In</a>
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            <Link to="/signin">Sign In</Link>
           </div>
         </header>
 
         <main>
           <Routes>
             <Route path="/cart" element={<CartScreen />}></Route>
-            <Route path="/cart/product/:id/qty/:qty" element={<CartScreen />}></Route>
+            <Route
+              path="/cart/product/:id/qty/:qty"
+              element={<CartScreen />}
+            ></Route>
             <Route path="/product/:id" element={<ProductScreen />}></Route>
-            <Route exact path="/" element={<HomeScreen />}></Route>
+            <Route path="/" element={<HomeScreen />}></Route>
           </Routes>
         </main>
 
--- amazona/frontend/src/index.js
+++ amazona/frontend/src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 import { Provider } from "react-redux";
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import store from './store';
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import store from "./store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <React.StrictMode>
