2e7f1cd5edc817e368f52e0ece7fb4aeee4e90cd
Amir Gabay <amir2347@gmail.com>
2022-08-03 12:19:05 -0400

    PlaceOrderScreen

--- amazona/frontend/src/App.js
+++ amazona/frontend/src/App.js
@@ -5,6 +5,7 @@ import { signout } from "./actions/userActions";
 import CartScreen from "./screens/CartScreen";
 import HomeScreen from "./screens/HomeScreen";
 import PaymentMethodScreen from "./screens/PaymentMethodScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import ProductScreen from "./screens/ProductScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ShippingAddressScreen from "./screens/ShippingAddressScreen";
@@ -70,6 +71,7 @@ function App() {
             ></Route>
             <Route path="/shipping" element={<ShippingAddressScreen />}></Route>
             <Route path="/payment" element={<PaymentMethodScreen />}></Route>
+            <Route path="/placeorder" element={<PlaceOrderScreen />}></Route>
             <Route path="/" element={<HomeScreen />}></Route>
           </Routes>
         </main>
